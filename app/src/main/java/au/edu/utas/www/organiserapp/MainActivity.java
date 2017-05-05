9d17e3d7c62676fe5be2b5dae50410e03eb8f684
Elryano <ryanmazengarb@hotmail.com>
2017-05-05 15:17:28 +1000

    comment

--- app/src/main/java/au/edu/utas/www/organiserapp/MainActivity.java
+++ app/src/main/java/au/edu/utas/www/organiserapp/MainActivity.java
@@ -1,14 +1,104 @@
 package au.edu.utas.www.organiserapp;
 
+import android.content.Context;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Vector ;
+//import java.util.Collection;
+
+
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView todaysComms;
+
+    List<taskObject> myList = new ArrayList<taskObject>(); // list of tasks
+
+
+
+
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        
+
+        taskObject newTask0 = new taskObject();
+
+        newTask0.name = "a";
+        newTask0.description = "assignment0";
+
+        taskObject newtask1 = new taskObject();
+
+        newtask1.name = "b";
+        newtask1.description = "assignment1";
+
+        taskObject newTask2 = new taskObject();
+
+        newTask2.name = "c";
+        newTask2.description = "assignment2";
+
+
+
+       // newTask.dueDate = new Date(2017,05,02);
+
+        myList.add(newTask0);
+        myList.add(newtask1);
+        myList.add(newTask2);
+
+       // for (int i = 0; i < 3; i++){
+
+      //  Log.d("test",myList.get(i).name);
+      //  Log.d("test",myList.get(i).description);
+      //  }
+        
+        
+        
+        
+        
+        todaysComms = (ListView) findViewById(R.id.TodayCommitmentsList);
+                
+         taskViewAdapter adapter = new taskViewAdapter(this, android.R.layout.simple_list_item_1,myList)  ;
+         todaysComms.setAdapter(adapter);
+                
+                
+                
+    }
+}
+
+class taskObject {
+
+
+    String name;
+    String description;
+    //java.util.Date dueDate;
+
+}
+
+class taskViewAdapter extends ArrayAdapter<taskObject>
+{
+    Context mContext;
+    int layoutResourceID;
+    List<taskObject> data = null;
+    
+    public taskViewAdapter(Context mContext, int layoutResourceID, List<taskObject> data){
+        super(mContext, layoutResourceID,data);
+        this.layoutResourceID = layoutResourceID;
+        this.mContext = mContext;
+        this.data = data;
     }
 }
