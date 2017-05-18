fa473e5bee1dde88626746e7262540bc301c69f9
Elryano <ryanmazengarb@hotmail.com>
2017-05-18 10:55:50 +1000

    tmq page, added a button and defined the function which will calculate the score

--- app/src/main/java/au/edu/utas/www/organiserapp/TMQ.java
+++ app/src/main/java/au/edu/utas/www/organiserapp/TMQ.java
@@ -2,12 +2,40 @@ package au.edu.utas.www.organiserapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class TMQ extends AppCompatActivity {
+public class TMQ  extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tmq);
+
+
+        Button TmqCompleteBtn = (Button) findViewById(R.id.buttonTmq);
+
+        TmqCompleteBtn.setOnClickListener (new View.OnClickListener()
+
+        {
+
+            @Override
+            public void onClick (View v){
+                getScore();
+            }
+        });
+    }
+
+
+
+
+
+
+    public void getScore(){
+
+        Toast.makeText(TMQ.this, "function called Successfully", Toast.LENGTH_SHORT).show();
     }
 }
+
