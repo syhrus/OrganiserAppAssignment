a9d1906953f089c8ced496595e7931e03ef8f87c
syhrus <tugman1234@hotmail.com>
2017-05-20 20:17:34 +1000

    Modified: TaskEnterScreen.java -Can now add a date in dd/mm/yyyy format
    EditTask.java
    -Updated to work with new date function

--- app/src/main/java/au/edu/utas/www/organiserapp/EditTask.java
+++ app/src/main/java/au/edu/utas/www/organiserapp/EditTask.java
@@ -3,9 +3,13 @@ package au.edu.utas.www.organiserapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.SeekBar;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import static au.edu.utas.www.organiserapp.R.id.textView;
 
@@ -28,14 +32,25 @@ public class EditTask extends AppCompatActivity {
             titleText.setText(thisTask.name);
             descText = (EditText) findViewById(R.id.TaskDescriptionInput);
             descText.setText(thisTask.description);
-            //TODO: Add pickup for date
+            dateText = (EditText) findViewById(R.id.TaskDateInput);
+            dateText.setText("" + thisTask.dueDate.getDate() + "/" + (thisTask.dueDate.getMonth() + 1) + "/" + (thisTask.dueDate.getYear() + 1900));
             importance = (SeekBar) findViewById(R.id.ImportanceInput);
             importance.setProgress((int) thisTask.urgency);
         }
     }
 
     public void saveChanges(View view) {
-        thisTask.changeTask(titleText.getText().toString(), descText.getText().toString(), new java.util.Date(), importance.getProgress());
+        java.util.Date dueDate = thisTask.dueDate;
+        EditText dateField = (EditText) findViewById(R.id.TaskDateInput);
+        try {
+            SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+            dueDate = df.parse(dateField.getText().toString());
+            Log.d("Date", dueDate.toString());
+        }
+        catch(ParseException e) {
+            e.printStackTrace();
+        }
+        thisTask.changeTask(titleText.getText().toString(), descText.getText().toString(), dueDate, importance.getProgress());
         startActivity(new Intent(EditTask.this, MainActivity.class));
     }
     public void discardChanges(View view) {
--- app/src/main/java/au/edu/utas/www/organiserapp/TaskEnterScreen.java
+++ app/src/main/java/au/edu/utas/www/organiserapp/TaskEnterScreen.java
@@ -3,9 +3,14 @@ package au.edu.utas.www.organiserapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.SeekBar;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class TaskEnterScreen extends AppCompatActivity {
 
@@ -15,7 +20,7 @@ public class TaskEnterScreen extends AppCompatActivity {
         setContentView(R.layout.activity_task_enter_screen);
     }
 
-    public void createTask(View view){
+    public void createTask(View view) throws ParseException {
 
         EditText nameField = (EditText)findViewById(R.id.TaskNameInput);
         EditText descField = (EditText)findViewById(R.id.TaskDescriptionInput);
@@ -24,8 +29,17 @@ public class TaskEnterScreen extends AppCompatActivity {
 
         String name = nameField.getText().toString();
         String description = descField.getText().toString();
-        //TODO: Work out how to get the date correctly.
+
         java.util.Date dueDate = new java.util.Date();
+        try {
+            SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+            dueDate = df.parse(dateField.getText().toString());
+            Log.d("Date", dueDate.toString());
+        }
+        catch(ParseException e) {
+            e.printStackTrace();
+        }
+
         float urgency = importanceBar.getProgress();
 
         ((GlobalVariables) this.getApplication()).addNewTask(name, description, dueDate, urgency);
